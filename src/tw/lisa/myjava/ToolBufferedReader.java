0ee6863d23a05c374e95e827a5e22e1cf09947f3
LisaWan33 <116249214+LisaWan33@users.noreply.github.com>
2022-10-26 00:30:27 +0800

    init

---
+++ src/tw/lisa/myjava/ToolBufferedReader.java
@@ -0,0 +1,10 @@
+package tw.lisa.myjava;
+
+public class ToolBufferedReader {
+
+	public static void main(String[] args) {
+		// 55、
+
+	}
+
+}
---
+++ src/tw/lisa/myjava/ToolFileinputStream_FileOutputStream.java
@@ -0,0 +1,10 @@
+package tw.lisa.myjava;
+
+public class ToolFileinputStream_FileOutputStream {
+
+	public static void main(String[] args) {
+		// 48、49、50、51、52、53
+
+	}
+
+}
